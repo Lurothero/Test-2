d685443bb2d35611f9b2d3dfd63dc95109df2ffd
Rene Allen <reneallen75@gmail.com>
2021-10-28 14:36:20 -0600

    Revamped some forms basic forms no validation yet

--- test2.js
+++ test2.js
@@ -5,28 +5,7 @@ $(document).ready(() =>
 
 {
     
-    $('#contactForm').hide();
 
-    $("#contactButton").on('click',() => {
-        $('#contactForm').show(); 
-       });
-       
-       $("#cancel").on('click',() => {
-        $('.contactForm').hide(); 
-       });
-
-
-        
-    $('#signupForm').hide();
-
-    $("#contactButton").on('click',() => {
-        $('#signupForm').show(); 
-       });
-       
-       $("#cancel").on('click',() => {
-        $('.signupForm').hide(); 
-       });
-      
       
 
       
