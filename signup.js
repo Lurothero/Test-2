560a9a15722fb050f609dbcf22c8260dbcf87dd1
Rene Allen <reneallen75@gmail.com>
2021-10-29 10:45:17 -0600

    95% done

---
+++ signup.js
@@ -0,0 +1,5 @@
+function signup (form)
+	 {
+    		var testvar = form.signUpFirstName.value
+    		alert ("you typed: " + testvar);
+	}
--- test2.js
+++ test2.js
@@ -1,13 +1,157 @@
-$(document).ready(() => 
+
+
+$(document).ready(function(){
+   
+
+$("html").hide();
+
+
+
+$("html").fadeIn(2000);
 
 
 
 
-{
-    
 
-      
 
-      
+
+
+
+
+
+
+$("#contactForm").hide();
+
+
+$("#contactButton").click(function(){
+
+
+
+
+
+    $("#contactForm").slideToggle();
+
+});
+
+
+$("#cancelContact").click(function(){
+
+   
+    $("#contactForm").slideUp();
+
+
+});
+
+
+
+$("#signupForm").hide();
+
+
+$("#signupButton").click(function(){
+
+    $("#signupForm").slideToggle();
+
+});
+
+
+$("#cancelSignup").click(function(){
+
+   
+    $("#signupForm").slideUp();
+
+
+});
+
+
+
+
+$("#card_tutor").hover(function(){
+
+    $("#card_tutor").animate({
+  
+        height: '105%',
+        width: '105%'
+      });
+
+    $("#card_tutor").css({"background-color": "#b4b4b4"});
+
+
+},
+function(){
+
+    $("#card_tutor").animate({
+  
+        height: '100%',
+        width: '100%'
+      });
+
+      $("#card_tutor").css({"background-color": "#FFFFFF"});
+
+}
+
+
+
+
+);
+
+$("#card_Assist").hover(function(){
+
+    $("#card_Assist").animate({
+  
+        height: '105%',
+        width: '105%'
+      });
+
+    $("#card_Assist").css({"background-color": "#b4b4b4"});
+
+
+},
+function(){
+
+    $("#card_Assist").animate({
+  
+        height: '100%',
+        width: '100%'
+      });
+
+      $("#card_Assist").css({"background-color": "#FFFFFF"});
+
+}
+
+
+
+
+);
+
+
+
+
+$("#card_Special").hover(function(){
+
+    $("#card_Special").animate({
+  
+        height: '105%',
+        width: '105%'
+      });
+
+    $("#card_Special").css({"background-color": "#b4b4b4"});
+
+
+},
+function(){
+
+    $("#card_Special").animate({
+  
+        height: '100%',
+        width: '100%'
+      });
+
+      $("#card_Special").css({"background-color": "#FFFFFF"});
+
+}
+
+
+);
+
 
 });
